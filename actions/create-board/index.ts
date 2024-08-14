4502f31677ccd135bbfb5cb9c60571aa488e2fe6
Muskan Sharma <muskan>
2024-08-14 17:31:23 +0530

    Stripe,Pro and Billing Portal

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,6 +8,9 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateBoard } from "./schema";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { incrementAvailableCount,hasAvailableCount } from "@/lib/org-limit";
+import { checkSubscription } from "@/lib/subscription";
+
 const handler =async(data:InputType):Promise<ReturnType>=>{
     const {userId,orgId}=auth();
     if(!userId|| !orgId){
@@ -15,6 +18,16 @@ const handler =async(data:InputType):Promise<ReturnType>=>{
             error:"Unauthorized"
         };
     }
+
+    const canCreate=await hasAvailableCount();
+    const isPro=await checkSubscription();
+
+    if(!canCreate&&!isPro){
+        return{
+            error:"You have reached your limit of free boards. Please upgrade to create more."
+        }
+    }
+
     const { title,image } =data;
     const [
         imageId,
@@ -42,7 +55,11 @@ const handler =async(data:InputType):Promise<ReturnType>=>{
                 imageLinkHTML,
             }
         });
-    
+
+    if(!isPro){
+        await incrementAvailableCount();
+    }
+        
     await createAuditLog({
         entityTitle:board.title,
         entityId:board.id,
--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -6,6 +6,10 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 import { Skeleton } from "@/components/ui/skeleton";
+import { getAvailableCount } from "@/lib/org-limit";
+import { MAX_FREE_BOARDS } from "@/constants/boards";
+import { checkSubscription } from "@/lib/subscription";
+
 export const BoardList=async()=>{
     const { orgId } = auth();
     if(!orgId){
@@ -19,6 +23,9 @@ export const BoardList=async()=>{
             createdAt:"desc"
         }
     });
+
+    const availableCount=await getAvailableCount();
+    const isPro=await checkSubscription();
 
     return(
         <div className="space-y-4">
@@ -50,7 +57,9 @@ export const BoardList=async()=>{
                 className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
                     <p className="text-sm">Create new board</p>
                     <span className="text-xs">
-                        5 remaining
+                        {isPro
+                        ?"Unlimited"
+                        :`${MAX_FREE_BOARDS-availableCount} remaining`}
                     </span>
                     <Hint
                     sideOffset={40}
